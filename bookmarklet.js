02673266a394cda2a269201cd9647a24fe903312
Mailo Svetel <development@rooland.cz>
2015-09-13 21:32:27 +0200

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+# Bookmarklet ready
+javascript:var%20a%20=%20document.getElementsByTagName('iframe');for(var%20i=0;i%20<%20a.length;i++)%20{a[i].parentNode.removeChild(a[i]);}
+
+# Raw JS
+var a = document.getElementsByTagName('iframe');for(var i=0;i < a.length;i++) {a[i].parentNode.removeChild(a[i]);}
